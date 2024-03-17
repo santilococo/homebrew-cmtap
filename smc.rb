e9974008a58b434086725fd7d75a0656271178db
Santiago Lo Coco <santilococo.01@gmail.com>
2024-03-17 15:53:44 +0100

    Add smc checksum

--- smc.rb
+++ smc.rb
@@ -2,7 +2,7 @@ class Smc < Formula
   desc "smc tool"
   homepage "https://github.com/santilococo/smc"
   url "https://github.com/santilococo/smc/archive/refs/tags/v0.03.tar.gz"
-  sha256 ""
+  sha256 "e0c6ba2bdad2d7260a61e8df32bde03f61108a7a7e6e775f053832f5424de8ba"
   license "GPL-2.0"
 
   def install
