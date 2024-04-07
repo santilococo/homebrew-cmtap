8b826277c02c68984b0ae5335052f21d77ee78d1
Santiago Lo Coco <santilococo.01@gmail.com>
2024-04-07 20:38:06 +0200

    Fix style issues

--- Formula/smc.rb
+++ Formula/smc.rb
@@ -1,5 +1,5 @@
 class Smc < Formula
-  desc "smc tool"
+  desc "System Management Controller (SMC) tool"
   homepage "https://github.com/santilococo/smc"
   url "https://github.com/santilococo/smc/archive/refs/tags/v0.03.tar.gz"
   sha256 "e0c6ba2bdad2d7260a61e8df32bde03f61108a7a7e6e775f053832f5424de8ba"
