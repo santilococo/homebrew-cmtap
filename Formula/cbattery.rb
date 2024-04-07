07efb24d168c0fafdd93b091522dcdbe91392712
Santiago Lo Coco <santilococo.01@gmail.com>
2024-04-07 20:36:03 +0200

    Update cbattery.rb

--- Formula/cbattery.rb
+++ Formula/cbattery.rb
@@ -1,5 +1,5 @@
 class Cbattery < Formula
-  desc "Manage battery-related settings on Apple Silicon systems."
+  desc "Manage battery-related settings on Apple Silicon systems"
   homepage "https://git.slc.ar/slococo/cbattery"
   url "https://git.slc.ar/slococo/cbattery/archive/v0.8.tar.gz"
   sha256 "f3681dafa8dfef4203615e33f89c57d69c54a5dd37d6c0d50b92be089b909940"
@@ -10,14 +10,15 @@ class Cbattery < Formula
 
   def install
     system "make", "PREFIX=#{prefix}", "install"
-    (pkgshare).install "#{buildpath}/.visudo"
+    pkgshare.install "#{buildpath}/.visudo"
     inreplace "#{bin}/cbattery", /\.visudo/, "#{opt_pkgshare}/.visudo"
   end
 
-  def caveats; <<~EOS
-    If you want to run the script without entering a password, execute:
+  def caveats
+    <<~EOS
+      If you want to run the script without entering a password, execute:
 
-      cbattery visudo
+        cbattery visudo
     EOS
   end
 
