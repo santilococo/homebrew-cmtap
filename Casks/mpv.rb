3ae68d90d8d623a88dfad0c1531282fa39ab6b8a
Santiago Lo Coco <santilococo.01@gmail.com>
2024-04-09 01:22:34 +0200

    Update mpv.rb

--- Casks/mpv.rb
+++ Casks/mpv.rb
@@ -1,18 +1,23 @@
 cask "mpv" do
   version "0.37.0"
-  sha256 "36cfa8dc112d58410e9b4296b038889e1d9154b6b97eb4a62efef7ca1d35e406"
+  sha256 "74e9bc0ac4d5476ac83c6c516bf000523ae58a688c21ab10f5503692db7ab0bf"
 
-  url "https://github.com/santilococo/cmbuilder/releases/download/mpv-v#{version}/mpv.dmg"
+  url "https://github.com/santilococo/cmbuilder/releases/download/mpv-v#{version}/mpv.tar.gz"
   name "mpv"
   desc "Media player based on MPlayer and mplayer2"
   homepage "https://github.com/santilococo/cmbuilder"
+
+  livecheck do
+    url :url
+    regex(/^mpv[._-]v?(\d+(?:\.\d+)+)$/i)
+  end
 
   conflicts_with formula: "mpv"
   depends_on macos: ">= :mojave"
 
   app "mpv.app"
   binary "#{appdir}/mpv.app/Contents/MacOS/mpv"
-  manpage "documentation/man/mpv.1"
+  manpage "mpv.1"
 
   zap trash: [
     "~/.config/mpv",
