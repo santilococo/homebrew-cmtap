d5b7807904c361f2c2c41cdfbdf6210bc4fbe250
Santiago Lo Coco <mail@slococo.com.ar>
2026-04-20 18:15:05 +0200

    Add livecheck

--- Formula/cliphist.rb
+++ Formula/cliphist.rb
@@ -1,11 +1,16 @@
 class Cliphist < Formula
   desc "Clipboard history tool"
   homepage "https://github.com/sentriz/cliphist"
-  url "https://github.com/sentriz/cliphist/releases/download/v0.7.0/v0.7.0-linux-amd64"
   version "0.7.0"
+  url "https://github.com/sentriz/cliphist/releases/download/v#{version}/v#{version}-linux-amd64"
   sha256 "d21ca6846bd25f7bd2dabb157912e2e07226db76be32885bc53d7f27db98d08a"
 
   def install
     bin.install "v#{version}-linux-amd64" => "cliphist"
+  end
+
+  livecheck do
+    url :stable
+    strategy :github_latest
   end
 end
