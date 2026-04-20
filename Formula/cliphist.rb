d9087c3bfcba7aeef88f8bf2824f471a70a15283
Santiago Lo Coco <mail@slococo.com.ar>
2026-04-20 18:49:53 +0200

    Test upgrade of cliphist

--- Formula/cliphist.rb
+++ Formula/cliphist.rb
@@ -1,9 +1,9 @@
 class Cliphist < Formula
   desc "Clipboard history tool"
   homepage "https://github.com/sentriz/cliphist"
-  version "0.7.0"
+  version "0.6.1"
   url "https://github.com/sentriz/cliphist/releases/download/v#{version}/v#{version}-linux-amd64"
-  sha256 "d21ca6846bd25f7bd2dabb157912e2e07226db76be32885bc53d7f27db98d08a"
+  sha256 "cf614f36e4b4d0128544df13d282138d65460a436db6baa750bdcfaa382bcb7d"
 
   def install
     bin.install "v#{version}-linux-amd64" => "cliphist"
